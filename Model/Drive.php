ee504f27d58ad46cf193d453118ea29140391daa
Mithun Das <mithun.programmer@gmail.com>
2014-04-14 00:28:30 +0530

    driver model updated

--- Model/Drive.php
+++ Model/Drive.php
@@ -6,7 +6,8 @@ set_include_path(APP . "Plugin" . DS . "GoogleApi" . DS . "Vendor" . DS . PATH_S
 
 require_once 'Google/Client.php';
 require_once 'Google/Service/Calendar.php';
-require 'Google/Service/Drive.php';
+require_once 'Google/Http/MediaFileUpload.php';
+require_once 'Google/Service/Drive.php';
 
 class Drive extends GoogleApiAppModel {
 
@@ -36,8 +37,11 @@ class Drive extends GoogleApiAppModel {
         CakeSession::write('GoogleApi.clientSecret', 'bZUDiyPXsQDdjGBWn8os-r-3');
         CakeSession::write('GoogleApi.clientRedirectUri', 'http://local.plugindev.com/google_api/drives');
     }
-
-    function getAllFiles() {
+/**
+     * get all files
+     * @return array
+     */
+    public function getAllFiles() {
         $result = array();
         $pageToken = NULL;
 
@@ -59,8 +63,11 @@ class Drive extends GoogleApiAppModel {
         } while ($pageToken);
         return $result;
     }
-
-    function getAllFolder() {
+    /**
+     * get all folders
+     * @return array
+     */
+    public function getAllFolder() {
         $result = array();
         $pageToken = NULL;
 
@@ -82,8 +89,12 @@ class Drive extends GoogleApiAppModel {
         } while ($pageToken);
         return $result;
     }
-
-    function getFile($fileId) {
+    /**
+     * get a file details
+     * @param int $fileId
+     * @return boolean
+     */
+    public function getFile($fileId) {
         try {
             $file = $this->service->files->get($fileId);
 
@@ -94,8 +105,12 @@ class Drive extends GoogleApiAppModel {
 
         return FALSE;
     }
-
-    function downloadFile($file) {
+    /**
+     * download a file
+     * @param int $file
+     * @return mixed
+     */
+    public function downloadFile($file) {
         $downloadUrl = $file->getDownloadUrl();
         if ($downloadUrl) {
             $request = new Google_HttpRequest($downloadUrl, 'GET', null, null);
@@ -111,8 +126,70 @@ class Drive extends GoogleApiAppModel {
             return null;
         }
     }
+    /**
+     * upload file to drive
+     * @param string $title
+     * @param string $description
+     * @param int $parentId
+     * @param string $mimeType
+     * @param string $filename
+     * @return array
+     */
+    public function uploadFile($title, $description, $parentId, $mimeType, $filename) {
+        try {
+            $file = new Google_Service_Drive_DriveFile();
+            $file->title = $title;
+            $chunkSizeBytes = 1 * 1024 * 1024;
+            // Call the API with the media upload, defer so it doesn't immediately return.
+            $this->client->setDefer(true);
+            $request = $this->service->files->insert($file);
 
-    function getParents($fileId = 0) {
+            // Create a media file upload to represent our upload process.
+            $media = new Google_Http_MediaFileUpload(
+                    $this->client, $request, $mimeType, null, true, $chunkSizeBytes
+            );
+            $media->setFileSize(filesize($filename));
+
+            // Upload the various chunks. $status will be false until the process is
+            // complete.
+            $status = false;
+            $handle = fopen($filename, "rb");
+            while (!$status && !feof($handle)) {
+                $chunk = fread($handle, $chunkSizeBytes);
+                $status = $media->nextChunk($chunk);
+            }
+
+            // The final value of $status will be the data from the API for the object
+            // that has been uploaded.
+            $result = false;
+            if ($status != false) {
+                $result = $status;
+            }
+
+            fclose($handle);
+            return $result;
+        } catch (Exception $e) {
+            print "An error occurred: " . $e->getMessage();
+        }
+    }
+    /**
+     * delete file in drive
+     * @param int $fileId
+     * @return resource
+     */
+    public function deleteFile($fileId) {
+        try {
+            return $this->service->files->delete($fileId);
+        } catch (Exception $e) {
+            print "An error occurred: " . $e->getMessage();
+        }
+    }
+    /**
+     * get parent (folder architecture of the OS)
+     * @param int $fileId
+     * @return array
+     */
+    public function getParents($fileId = 0) {
         $results = array();
         try {
             $parents = $this->service->parents->listParents($fileId);
@@ -125,8 +202,12 @@ class Drive extends GoogleApiAppModel {
         }
         return $parent;
     }
-
-    function getFilesByFolder($folderId) {
+    /**
+     * get file list by folders
+     * @param int $folderId
+     * @return array
+     */
+    public function getFilesByFolder($folderId) {
         $pageToken = NULL;
         $result = array();
         do {
@@ -147,17 +228,26 @@ class Drive extends GoogleApiAppModel {
         } while ($pageToken);
         return  $result;
     }
-
+    /**
+     * create url
+     * @return string
+     */
     public function createUrl() {
         return $this->client->createAuthUrl();
     }
-
+    /**
+     * authenticate api data
+     * @param string $code
+     */
     public function authenticateApi($code) {
         $this->client->authenticate($code);
         $accessToken = $this->client->getAccessToken();
         CakeSession::write('GoogleApi.access_token', $accessToken);
     }
-
+    /**
+     * set access token
+     * @param mixed $accessToken
+     */
     public function setAccessToken($accessToken) {
         $this->client->setAccessToken($accessToken);
     }
